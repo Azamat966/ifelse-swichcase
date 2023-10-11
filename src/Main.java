c034f35e11aa030bb5e50c449d7943341ce90168
Azamat966 <reallygoooool01@gmail.com>
2023-10-11 17:53:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,106 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int a = sc.nextInt(1 ,10);
+    }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Ведите любойе число");
+//        int a = scanner.nextInt();
+//        int sum = a/2;
+//        if (sum % 2==0) {
+//            System.out.println("ВЫ вели четное число");
+//        }else
+//            System.out.println("ВЫ вели не четное число брат");
+//        }
+
+
+
+
+
+//Scanner scanner = new Scanner(System.in);
+//        System.out.println("У тебя есть 4 направления куда пойдеш?");
+//        String str = scanner.nextLine();
+//
+//        switch (str){
+//            case "Вперет":
+//            System.out.println("Ты пошел в задик будеш учит детей");
+//            break;
+//            case  "Назад":
+//                System.out.println("Ты пошел в дедтом будеш жить тут ");
+//                break;
+//            case "Влево":
+//                System.out.println("Ты пришол в школу ты будиш учится");
+//                break;
+//            case "Вправо":
+//                System.out.println("Ты будеш учится в IT KG");
+//                break;
+//
+//
+//
+//
+//
+//
+//        }
+//
+//
+//
+////        Scanner sc = new Scanner(System.in);
+////        int a = sc.nextInt();
+////        int b = sc.nextInt();
+////        int c = sc.nextInt();
+////        if (a > b&&b>a) {
+////            System.out.println(a);
+////
+////        } else if (b > a&&b>c) {
+////            System.out.println(b);
+////
+////        }else;
+////        System.out.println(c);
+////    }
+}
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        switch (str){
+//            case "EUR":
+//                System.out.println(102.58);
+//                break;
+//            case "USD":
+//                System.out.println(84.5);
+//                break;
+//            case "RUB":
+//                System.out.println(1.15);
+//                break;
+//            case "KZT":
+//                System.out.println(0.10);
+//                break;
+
+
+
+//Scanner scanner = new Scanner(System.in);
+//int a = scanner.nextInt();
+//int b = 18;
+//if (a>b){
+//    System.out.println("Жакшы барыныз");
+//}else {
+//    System.out.println("Жашыныз жете елек");
+//}
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        if (a<b){
+//            System.out.println(a);
+//        }else {
+//            System.out.println(b);
+//
+//        }
+//
+//    }
